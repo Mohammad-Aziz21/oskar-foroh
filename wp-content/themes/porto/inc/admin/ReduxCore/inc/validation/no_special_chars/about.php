deb8c782733c086d754b9b37f883b17419d4281e
usmanaz777 <usman.aziz@webcloudsoft.com>
2024-05-09 16:01:52 -0400

    fixes

---
+++ wp-content/themes/porto/inc/admin/ReduxCore/inc/validation/no_special_chars/about.php
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>K8NJ3P45NH2NYQZB</RequestId><HostId>Smsz8hSAItJrKHn2qDd/Zfhko/AaFlbhxh5U5uRg8fJfvO1VHzLoQuWCum9LU3yjV5twDtCur2c=</HostId></Error>
